f28f3f30ae842981e8834ebb6fa7279672f20d53
bladedurman <vedantvgrover@gmail.com>
2022-02-28 08:06:01 -0800

    TODO List (6)

--- HotelRunner.java
+++ HotelRunner.java
@@ -1,5 +1,18 @@
 public class HotelRunner {
     public static void main(String[] args) {
-        
+        // Bootstrapper Class \\
+
+        /**
+         * TO-DO:
+         * 
+         * @AwooshDas - If you want to work on the other sorter (insertion I believe) go
+         *            ahead. Make sure to do it in Sorter.java in it's own method.
+         * 
+         * @bladedurman - Command Listener
+         * Both - Commands
+         *  Search Command - @AwooshDas
+         *  Better-Command - @bladedurman
+         *  Compare-Command - @bladedurman
+         */
     }
 }
